53940c9d98fa01008348360bce8f1d641b04dfd4
Mona <monabadee@gmail.com>
2023-11-22 20:52:37 +0300

    Complete  Lab

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+  
+  console.log(books); 
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+
+function fetchBooks() {
+  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json)); 
+}
+
+
+document.addEventListener("DOMContentLoaded", fetchBooks);
